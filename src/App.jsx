c119a2e3cf3dca61a70456087cd9659a1da55236
leoantony2002 <leoantony20025@gmail.com>
2024-03-08 09:04:31 +0530

    new start

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import TermsConditions from "./pages/terms-conditions";
 import PrivacyPolicy from "./pages/privacy-policy";
+import Resource from "./pages/Resource";
+import Mission from "./pages/Mission";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />{" "}
           <Route path="/terms-conditions" element={<TermsConditions />} />{" "}
+          <Route path="/misson" element={<Mission />} />{" "}
+          <Route path="/resources" element={<Resource />} />{" "}
           <Route path="/contact" element={<Contact />} />{" "}
           <Route path="/about" element={<About />} />{" "}
           <Route path="/" element={<Home />} />{" "}
---
+++ src/pages/Mission.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Mission.css"
+
+const Mission = () => {
+  return (
+    <div>Mission</div>
+  )
+}
+
+export default Mission
